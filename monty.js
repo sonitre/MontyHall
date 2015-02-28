f840ca725c688a5fc4833d0ce8048d280b3ea764
Sonia Trehan <soniatrehan6@gmail.com>
2015-02-28 14:19:50 -0500

    Added games as a parameter

--- monty.js
+++ monty.js
@@ -1,12 +1,19 @@
-function MontyHall(){
+//Javascript file that proves the Monty Hall Problem. Creates a Monty Hall object and allows
+//a user to run the game as many times as they would like!
+
+//Constructs a MontyHall object
+function MontyHall(games){
 	this.prize;
 	this.userChoice;
 	this.hostChoice;
 	this.switchChoice;
 	this.firstChoiceWin = 0;
 	this.switchChoiceWin = 0;
+	this.games = games;
 };
 
+//Method that randomly chooses a door between 1 and 3, 
+//taking into account constraints (such as doors already being assigned).
 MontyHall.prototype.pickDoor = function(constraint1, constraint2){
 	var door = Math.floor(Math.random() * 3);
 	while(door === constraint1 || door === constraint2){
@@ -15,13 +22,15 @@ MontyHall.prototype.pickDoor = function(constraint1, constraint2){
 	return door;
 };
 
-MontyHall.prototype.run1000 = function(){
-	for(var i = 0; i < 1000; i++){
+//Assigns the prize to a door, has the user choose a door,
+//and then has the host show the door without the prize
+MontyHall.prototype.runMonty = function(){
+	for(var i = 0; i < this.games; i++){
 		this.prize = this.pickDoor();
 		this.userChoice = this.pickDoor();
 		this.hostChoice = this.pickDoor(this.prize, this.userChoice);
 		this.switchChoice = this.pickDoor(this.userChoice, this.hostChoice);
-
+		//Logs how many tiems switching wins vs. sticking with first choice
 		if(this.userChoice === this.prize){
 			this.firstChoiceWin++;
 		}
@@ -31,11 +40,13 @@ MontyHall.prototype.run1000 = function(){
 	}
 };
 
+//Logs percentage of wins to the console
 MontyHall.prototype.logResults = function(){
-	console.log("Sticking with the first choice won " + (100 * this.firstChoiceWin/1000) + "% of the time.");
-	console.log("And switching choices won " + (100 * this.switchChoiceWin/1000) + "% of the time!");
+	console.log("Sticking with the first choice won " + (100 * this.firstChoiceWin/this.games) + "% of the time.");
+	console.log("And switching choices won " + (100 * this.switchChoiceWin/this.games) + "% of the time!");
 };
 
-var monty = new MontyHall();
-monty.run1000();
+//Creates a new MontyHall object and runs it 1000 times
+var monty = new MontyHall(1000);
+monty.runMonty();
 monty.logResults();
